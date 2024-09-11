2030032e05abfa0d0eccd38919c6796d1dbcf868
Ronaldo Setúbal <113723186+ro090370@users.noreply.github.com>
2024-09-11 02:32:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade da Rayssa], ela já coleciona diversos títulos e é considerada uma das maiores promessas do esporte. Sua história de superação e sua paixão pelo skate a transformaram em um ícone para a nova geração.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. Com [idade da Beatriz], ela já conquistou diversas medalhas em competições nacionais e internacionais, representando o Brasil com muita garra. Sua dedicação aos treinos e sua paixão pelo judô a colocam como uma das principais atletas da modalidade no país.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com suas acrobacias e beleza. Aos [idade da Rebeca], ela já conquistou diversos títulos e é considerada uma das maiores ginastas da história do Brasil. Sua superação após uma lesão grave a tornou um exemplo de determinação e força de vontade.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
